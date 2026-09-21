551ea3ccab27193f9f3b2834f5f2d6e35b04d54a
Quezzie <chloevalesquez@gmail.com>
2026-09-21 02:05:12 +0000

    chore: untrack rust_chimera/target build cache, sync docs monograph PDF to v2

--- rust_chimera/target/debug/deps/rust_chimera-940b518a09793fae.d
+++
@@ -1,7 +0,0 @@
-/Users/chloe/Desktop/Developer/Project Chimera/rust_chimera/target/debug/deps/rust_chimera-940b518a09793fae.d: src/lib.rs
-
-/Users/chloe/Desktop/Developer/Project Chimera/rust_chimera/target/debug/deps/librust_chimera-940b518a09793fae.rlib: src/lib.rs
-
-/Users/chloe/Desktop/Developer/Project Chimera/rust_chimera/target/debug/deps/librust_chimera-940b518a09793fae.rmeta: src/lib.rs
-
-src/lib.rs:
--- rust_chimera/target/debug/librust_chimera.d
+++
@@ -1,1 +0,0 @@
-/Users/chloe/Desktop/Developer/Project\ Chimera/rust_chimera/target/debug/librust_chimera.rlib: /Users/chloe/Desktop/Developer/Project\ Chimera/rust_chimera/src/lib.rs
